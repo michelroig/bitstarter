42f2f937399e3dc127aeaa381bbcf5b6331f1bfe
Michel Roig <josselin99@gmail.com>
2013-07-10 13:21:40 +0000

    Updated web.js so that it send back a String instead of a buffer

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var fs = require('fs');
-
+var buf = new Buffer(256);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html"));
+  response.send(buf.toString(,fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
